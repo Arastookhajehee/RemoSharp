f7db44d95d24c1ee161b1ef8ebacae7db80c52fc
Arastoo Khajehee <64447672+Arastookhajehee@users.noreply.github.com>
2024-06-08 18:25:47 +0900

    Authorship Credential update from a default value to Arastoo Khajehee Added installer script

--- RemoSharp/RemoSharpInfo.cs
+++ RemoSharp/RemoSharpInfo.cs
@@ -26,7 +26,7 @@ namespace RemoSharp
             get
             {
                 //Return a short string describing the purpose of this GHA library.
-                return "";
+                return "RemoSharp, a tool for real-time collaborative computational design and digital fabrication";
             }
         }
         public override Guid Id
@@ -42,7 +42,7 @@ namespace RemoSharp
             get
             {
                 //Return a string identifying you or your company.
-                return "HP Inc.";
+                return "Arastoo Khajehee";
             }
         }
         public override string AuthorContact
@@ -50,7 +50,7 @@ namespace RemoSharp
             get
             {
                 //Return a string representing your preferred contact details.
-                return "";
+                return "a.khajehee@gmail.com";
             }
         }
     }
